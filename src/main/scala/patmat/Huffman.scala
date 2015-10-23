c0f9d5010227ed2b2e48ec7348f3da3ab1a0ec02
tayazhurba <taisiya_zhurba@mail.ru>
2015-10-24 03:52:58 +0800

    go onnn

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -2,6 +2,8 @@ package patmat
 
 import common._
 
+import scala.collection.immutable.Nil
+
 object Huffman {
 
   /**
@@ -85,11 +87,11 @@ object Huffman {
     def singleton(trees: List[CodeTree]): Boolean = {
     trees.size == 1
   }
-  
+
   /**
    * Параметр `trees` этой функции - список деревьев кода, упорядоченных по возрастанию весов.
    *
-   * Эта функция берет два первых элемента списка `trees` и комбинирует их в отдельный узел 
+   * Эта функция берет два первых элемента списка `trees` и комбинирует их в отдельный узел
    * `Fork`. Этот узел затем добавляется обратно к оставшимся элементам
    * `trees` в позицию, так чтобы сохранить порядок возрастания весов.
    *
@@ -99,7 +101,7 @@ object Huffman {
     case left::right::chars => (makeCodeTree(left, right)::chars).sortWith((elem1,elem2) => weight(elem1) < weight(elem2))
     case _ => trees
   }
-  
+
   /**
    * Эта функция будет вызвана следующим образом: until(singleton, combine)(trees)
    *
@@ -118,43 +120,29 @@ object Huffman {
       if (single(trees)) trees
       else until(single,listTrees)(combine(trees))
   }
-  
-  /**
-   * ЭТа функция создает дерева кода , оптимальное для кодирования текста `chars`.
-   *
-   * Параметр `chars` произвольный текст. Эта функция извлекает последовательность символов из текста и создает дерево кода
-   * базирующегося на нем.
-   */
-    def createCodeTree(chars: List[Char]): CodeTree = ???
-  
 
-  // Часть 3: Декодирование
+    def createCodeTree(chars: List[Char]): CodeTree = until(singleton, combine)(makeOrderedLeafList(times(chars))).head
 
   type Bit = Int
 
-  /**
-   * Эта функция декодирует последовательность битов `bits`, используя дерево кодов `tree` и возвращает
-   * результирующий список символов.
-   */
-    def decode(tree: CodeTree, bits: List[Bit]): List[Char] = ???
-  
-  /**
-   * Дерево кодирования Хафмана для Французского языка.
-   * Сгенерированно по данным из
-   *   http://fr.wikipedia.org/wiki/Fr%C3%A9quence_d%27apparition_des_lettres_en_fran%C3%A7ais
-   */
+    def decode(tree: CodeTree, bits: List[Bit]): List[Char] = {
+      def decode1(chars: List[Char], curTree: CodeTree, bits: List[Bit]): List[Char] = curTree match{
+        case Leaf(c,_) if (bits.isEmpty) => (c::chars).reverse
+        case Leaf(c,_) => decode1(c::chars, tree, bits)
+        case Fork(l,_,_,_) if (bits.head == 0) => decode1(chars, l, bits.tail)
+        case Fork(_,r,_,_) if (bits.head == 1) => decode1(chars, r, bits.tail)
+      }
+      decode1(Nil, tree, bits);
+    }
+
   val frenchCode: CodeTree = Fork(Fork(Fork(Leaf('s',121895),Fork(Leaf('d',56269),Fork(Fork(Fork(Leaf('x',5928),Leaf('j',8351),List('x','j'),14279),Leaf('f',16351),List('x','j','f'),30630),Fork(Fork(Fork(Fork(Leaf('z',2093),Fork(Leaf('k',745),Leaf('w',1747),List('k','w'),2492),List('z','k','w'),4585),Leaf('y',4725),List('z','k','w','y'),9310),Leaf('h',11298),List('z','k','w','y','h'),20608),Leaf('q',20889),List('z','k','w','y','h','q'),41497),List('x','j','f','z','k','w','y','h','q'),72127),List('d','x','j','f','z','k','w','y','h','q'),128396),List('s','d','x','j','f','z','k','w','y','h','q'),250291),Fork(Fork(Leaf('o',82762),Leaf('l',83668),List('o','l'),166430),Fork(Fork(Leaf('m',45521),Leaf('p',46335),List('m','p'),91856),Leaf('u',96785),List('m','p','u'),188641),List('o','l','m','p','u'),355071),List('s','d','x','j','f','z','k','w','y','h','q','o','l','m','p','u'),605362),Fork(Fork(Fork(Leaf('r',100500),Fork(Leaf('c',50003),Fork(Leaf('v',24975),Fork(Leaf('g',13288),Leaf('b',13822),List('g','b'),27110),List('v','g','b'),52085),List('c','v','g','b'),102088),List('r','c','v','g','b'),202588),Fork(Leaf('n',108812),Leaf('t',111103),List('n','t'),219915),List('r','c','v','g','b','n','t'),422503),Fork(Leaf('e',225947),Fork(Leaf('i',115465),Leaf('a',117110),List('i','a'),232575),List('e','i','a'),458522),List('r','c','v','g','b','n','t','e','i','a'),881025),List('s','d','x','j','f','z','k','w','y','h','q','o','l','m','p','u','r','c','v','g','b','n','t','e','i','a'),1486387)
 
-  /**
-   * О чем говорит секретноесообщение? Можете ли вы его декодировать?
-   * Для декодирования используйте дерево Хафмана `frenchCode'.
-   */
   val secret: List[Bit] = List(0,0,1,1,1,0,1,0,1,1,1,0,0,1,1,0,1,0,0,1,1,0,1,0,1,1,0,0,1,1,1,1,1,0,1,0,1,1,0,0,0,0,1,0,1,1,1,0,0,1,0,0,1,0,0,0,1,0,0,0,1,0,1)
 
   /**
    * Напишите функцию, которая возвращает декодированный секрет
    */
-    def decodedSecret: List[Char] = ???
+    def decodedSecret: List[Char] = decode(frenchCode, secret)
   
 
   // Часть 4a: Кодирование с использованием дерева Хафмана
@@ -163,8 +151,20 @@ object Huffman {
    * ЭТа функция кодирует `text` с помощью кодового дерева `tree` в
    * последовательность битов.
    */
-    def encode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
-  
+    def encode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+      def encode1(curTree: CodeTree, bits: List[Bit])(text: List[Char]): List[Bit] = curTree match{
+        case Leaf(c,_) if (text.tail == Nil) => bits.reverse
+        case Leaf(c,_) => encode1(tree, bits)(text.tail)
+        case Fork(l,r,_,_) if (chars(l).contains(text.head)) => encode1(l,0::bits)(text)
+        case Fork(l,r,_,_) => encode1(r,1::bits)(text)
+      }
+      encode1(tree,Nil)(text)
+    }
+
+  val hc = Huffman.decodedSecret
+
+  def encodeSecret: List[Bit] = encode(frenchCode)(hc)
+
   // Часть 4b: Кодирование, использующее таблицу
 
   type CodeTable = List[(Char, List[Bit])]
@@ -200,3 +200,28 @@ object Huffman {
    */
     def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
   }
+
+object Main extends App{
+  println("hello =)")
+
+  // println(Huffman.makeOrderedLeafList(Huffman.times("hello".toList)))
+
+  val hc = Huffman.decodedSecret
+  println(hc)
+
+  val he = Huffman.encodeSecret
+  println(he)
+//
+//  val code = Huffman.encode(Huffman.frenchCode)(hc)
+//  println(code)
+//
+//  val table = Huffman.convert(Huffman.frenchCode)
+//  println(table)
+//
+//  val ch = Huffman.codeBits(table)('d')
+//  println(ch)
+//
+//  val q = Huffman.quickEncode(Huffman.frenchCode)("huffmanestcool".toList)
+//  println(q)
+
+}
